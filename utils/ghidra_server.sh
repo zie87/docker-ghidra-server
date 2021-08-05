ea9bc473895ff8561698ebde674fc2d79324daa2
Tobias Zindl <zie.develop@gmail.com>
2021-08-05 08:20:04 +0200

    add auto pull to start script

--- utils/ghidra_server.sh
+++ utils/ghidra_server.sh
@@ -1,7 +1,11 @@
 #!/bin/sh
 
+USERS="admin zie ghidra"
+REPO_PATH=/mnt/data/Workspace/ghidra-repos
+
 docker run --rm --name ghidra-server \
-	   -e GHIDRA_USERS="ubuntu another_user" \
-	   -v /usr/local/ghidra/ghidra_collab:/repos \
-	   -p 13100-13102:13100-13102 \
-	   zie87/ghidra-server:latest
+       -e GHIDRA_USERS="${USERS}" \
+       -v ${REPO_PATH}:/repos \
+       -p 13100-13102:13100-13102 \
+       --pull always \
+       zie87/ghidra-server:latest
